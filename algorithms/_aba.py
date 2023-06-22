501100f6d330ebcc2d5536ed1cdf12b6ca7d0731
emilyburnett2003 <118637936+emilyburnett2003@users.noreply.github.com>
2023-06-22 13:00:59 -0400

    fixing aba errors

--- algorithms/_aba.py
+++ algorithms/_aba.py
@@ -373,7 +373,7 @@ def gen_aba_kernel(self, use_thread_group = False, single_call_timing = False):
 
     # add shared memory variables
     # tau? qdd?
-    shared_mem_vars = ["__shared__ T s_qdd[" + str(n) + "];"
+    shared_mem_vars = ["__shared__ T s_qdd[" + str(n) + "];", \
                         "__shared__ T s_q_qd[2*" + str(n) + "]; T *s_q = s_q_qd; T *s_qd = &s_q_qd[" + str(n) + "];", \
                         "__shared__ T s_tau[" + str(n)"];"
                        "__shared__ T s_va[" + str(18*n) + "];"]
